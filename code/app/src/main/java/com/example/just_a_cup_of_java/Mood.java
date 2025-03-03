c6b7884a5116558b96fc507cc7797db0115abb34
Heena Kamani <hkamani@ualberta.ca>
2025-03-02 20:12:36 -0700

    User and Mood Class updates

---
+++ code/app/src/main/java/com/example/just_a_cup_of_java/Mood.java
@@ -0,0 +1,30 @@
+package com.example.just_a_cup_of_java;
+
+import com.example.just_a_cup_of_java.User;
+
+
+// constant emotional states
+
+public class Mood {
+    //constant emotional states
+    static String StateHappy = "Happy";
+    static String StateSad = "Sad";
+    static String StateAngry = "Angry";
+    static String StateSurprised = "Surprised";
+    static String StateNeutral = "Neutral";
+    static String StateConfused = "Confused";
+    static String StateOther = "Other";
+
+
+    // One of the State static strings of this class.
+    String Emotional_state = "";
+    String CustomState = "";
+    String Location = "";
+    String Trigger = "";
+    String Social_situation = "";
+    String Feeling_reason = "";
+    String Photo_url = "";
+
+    //pointer to the User
+    User Username = null;
+}
---
+++ code/app/src/main/java/com/example/just_a_cup_of_java/User.java
@@ -0,0 +1,12 @@
+package com.example.just_a_cup_of_java;
+
+public class User {
+
+    String Name = "";
+    String Email = "";
+    String Password = "";
+    String Bio = "";
+    String ProfilePic = "";
+
+    String Username = "";
+}
