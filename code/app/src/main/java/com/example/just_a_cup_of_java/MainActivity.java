2fe70cbf2567fdf04571333d7a21f3d50128afc7
Tinashe Masoka <tidorkthegreat@gmail.com>
2025-03-04 19:39:31 -0700

    completed the SocialSituation enum file

--- code/app/src/main/java/com/example/just_a_cup_of_java/MainActivity.java
+++ code/app/src/main/java/com/example/just_a_cup_of_java/MainActivity.java
@@ -8,7 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseFirestore db;
+    private CollectionReference usersRef;
+    private CollectionReference moodsRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        db = FirebaseFirestore.getInstance();
+        usersRef = db.collection("Users");
+        moodsRef = db.collection("Moods");
     }
+
+
 }
--- code/app/src/main/java/com/example/just_a_cup_of_java/SocialSituation.java
+++ code/app/src/main/java/com/example/just_a_cup_of_java/SocialSituation.java
@@ -1,4 +1,8 @@
 package com.example.just_a_cup_of_java;
 
-public class SocialSituation {
+public enum SocialSituation {
+    ALONE,
+    WITH_ONE_OTHER,
+    WITH_TWO_TO_SEVERAL,
+    WITH_A_CROWD
 }
