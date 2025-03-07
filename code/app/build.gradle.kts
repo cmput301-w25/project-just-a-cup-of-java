8d2f611758c0b74549f8a85581e9d14da19f8615
simba <skandemi@ualberta.ca>
2025-03-06 18:58:18 -0700

    add moodhistory.xml

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.just_a_cup_of_java"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.just_a_cup_of_java"
@@ -40,7 +40,10 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
     implementation("com.google.firebase:firebase-firestore:25.1.1")
     implementation("com.google.firebase:firebase-auth")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.prolificinteractive:material-calendarview:1.6.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
